541ca7a34f2f28b12120c097bda2b60f8a99b2d2
viperxc24odmaker <viperxc2204@gmail.com>
2026-08-15 17:08:48 +0100

    feat: expose Ely.by login to renderer

--- src/preload/index.ts
+++ src/preload/index.ts
@@ -1,21 +1,6 @@
 import { contextBridge, ipcRenderer } from 'electron'
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  minimize: () => ipcRenderer.send('window:minimize'),
-  maximize: () => ipcRenderer.send('window:maximize'),
-  close: () => ipcRenderer.send('window:close'),
-  getSettings: () => ipcRenderer.invoke('settings:get'),
-  setSetting: (key: string, value: unknown) => ipcRenderer.invoke('settings:set', key, value),
-  getVersion: () => ipcRenderer.invoke('app:getVersion'),
-  getMinecraftVersions: () => ipcRenderer.invoke('minecraft:versions'),
-  loginMicrosoft: () => ipcRenderer.invoke('minecraft:loginMicrosoft'),
-  addOfflineAccount: (username: string) => ipcRenderer.invoke('minecraft:addOffline', username),
-  getAccounts: () => ipcRenderer.invoke('minecraft:accounts'),
-  removeAccount: (id: string) => ipcRenderer.invoke('minecraft:removeAccount', id),
-  setActiveAccount: (id: string) => ipcRenderer.invoke('minecraft:setActiveAccount', id),
-  installJava: (version: string) => ipcRenderer.invoke('minecraft:installJava', version),
-  launchMinecraft: (input: unknown) => ipcRenderer.invoke('minecraft:launch', input),
-  getMods: (instanceId: string) => ipcRenderer.invoke('minecraft:mods', instanceId),
-  toggleMod: (instanceId: string, fileName: string, enabled: boolean) => ipcRenderer.invoke('minecraft:toggleMod', instanceId, fileName, enabled),
-  importMod: (instanceId: string) => ipcRenderer.invoke('minecraft:importMod', instanceId)
+contextBridge.exposeInMainWorld('electronAPI',{
+  minimize:()=>ipcRenderer.send('window:minimize'),maximize:()=>ipcRenderer.send('window:maximize'),close:()=>ipcRenderer.send('window:close'),
+  getSettings:()=>ipcRenderer.invoke('settings:get'),setSetting:(key:string,value:unknown)=>ipcRenderer.invoke('settings:set',key,value),getVersion:()=>ipcRenderer.invoke('app:getVersion'),
+  getMinecraftVersions:()=>ipcRenderer.invoke('minecraft:versions'),loginMicrosoft:()=>ipcRenderer.invoke('minecraft:loginMicrosoft'),loginElyBy:(username:string,password:string)=>ipcRenderer.invoke('minecraft:loginElyBy',username,password),addOfflineAccount:(username:string)=>ipcRenderer.invoke('minecraft:addOffline',username),getAccounts:()=>ipcRenderer.invoke('minecraft:accounts'),removeAccount:(id:string)=>ipcRenderer.invoke('minecraft:removeAccount',id),setActiveAccount:(id:string)=>ipcRenderer.invoke('minecraft:setActiveAccount',id),installJava:(version:string)=>ipcRenderer.invoke('minecraft:installJava',version),launchMinecraft:(input:unknown)=>ipcRenderer.invoke('minecraft:launch',input),getMods:(instanceId:string)=>ipcRenderer.invoke('minecraft:mods',instanceId),toggleMod:(instanceId:string,fileName:string,enabled:boolean)=>ipcRenderer.invoke('minecraft:toggleMod',instanceId,fileName,enabled),importMod:(instanceId:string)=>ipcRenderer.invoke('minecraft:importMod',instanceId)
 })
